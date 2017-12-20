d65071dc970065c09e2b84ff09a32000d8a14764
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-12-20 13:45:39 +0900

    [bugfix] fix thirdparty repo, bump rubocop and QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -4,13 +4,13 @@ require "serverspec"
 package = "argus-clients"
 service = "radium"
 config  = "/etc/radium.conf"
-ra_config = "/etc/ra.conf"
-user    = "radium"
+# ra_config = "/etc/ra.conf"
+user = "radium"
 argus_user = "argus"
 user_shell   = ""
 user_home    = ""
 group        = "argus"
-ports   = [ 561, 562 ]
+ports   = [561, 562]
 log_dir = "/var/log/argus"
 default_user = "root"
 default_group = "root"
@@ -31,7 +31,7 @@ when "openbsd"
 when "freebsd"
   package = "argus-clients-sasl"
   config = "/usr/local/etc/radium.conf"
-  ra_config = "/usr/local/etc/ra.conf"
+  #  ra_config = "/usr/local/etc/ra.conf"
   log_dir = "/var/log/argus"
   default_group = "wheel"
   user_home = "/var/log/argus"
@@ -40,7 +40,7 @@ end
 
 describe package(package) do
   it { should be_installed }
-end 
+end
 
 describe group(group) do
   it { should exist }
@@ -59,18 +59,18 @@ describe file(config) do
   it { should be_mode 644 }
   it { should be_owned_by default_user }
   it { should be_grouped_into default_group }
-  its(:content) { should match (/^RADIUM_DAEMON="#{ Regexp.escape(daemonized) }"$/) }
-  its(:content) { should match (/^RADIUM_MONITOR_ID="localhost"$/) }
-  its(:content) { should match (/^RADIUM_MAR_STATUS_INTERVAL=5$/) }
-  its(:content) { should match (/^RADIUM_ARGUS_SERVER="argus:\/\/localhost:561"$/) }
-  its(:content) { should match (/^RADIUM_FILTER="ip"/) }
-  its(:content) { should match (/^RADIUM_USER_AUTH="foo@reallyenglish\.com\/foo@reallyenglish\.com"$/) }
-  its(:content) { should match (/^RADIUM_AUTH_PASS="password"$/) }
-  its(:content) { should match (/^RADIUM_ACCESS_PORT=562$/) }
-  its(:content) { should match (/^RADIUM_BIND_IP="127\.0\.0\.1"/) }
-  its(:content) { should match (/^RADIUM_OUTPUT_FILE="#{ Regexp.escape("/var/log/argus/radium.out") }"$/) }
-  its(:content) { should match (/^RADIUM_SETUSER_ID="#{ Regexp.escape(user) }"$/) }
-  its(:content) { should match (/^RADIUM_SETGROUP_ID="#{ Regexp.escape(group) }"$/) }
+  its(:content) { should match(/^RADIUM_DAEMON="#{ Regexp.escape(daemonized) }"$/) }
+  its(:content) { should match(/^RADIUM_MONITOR_ID="localhost"$/) }
+  its(:content) { should match(/^RADIUM_MAR_STATUS_INTERVAL=5$/) }
+  its(:content) { should match(%r{^RADIUM_ARGUS_SERVER="argus://localhost:561"$}) }
+  its(:content) { should match(/^RADIUM_FILTER="ip"/) }
+  its(:content) { should match(%r{^RADIUM_USER_AUTH="foo@reallyenglish\.com/foo@reallyenglish\.com"$}) }
+  its(:content) { should match(/^RADIUM_AUTH_PASS="password"$/) }
+  its(:content) { should match(/^RADIUM_ACCESS_PORT=562$/) }
+  its(:content) { should match(/^RADIUM_BIND_IP="127\.0\.0\.1"/) }
+  its(:content) { should match(/^RADIUM_OUTPUT_FILE="#{ Regexp.escape("/var/log/argus/radium.out") }"$/) }
+  its(:content) { should match(/^RADIUM_SETUSER_ID="#{ Regexp.escape(user) }"$/) }
+  its(:content) { should match(/^RADIUM_SETGROUP_ID="#{ Regexp.escape(group) }"$/) }
 end
 
 describe file(log_dir) do
@@ -88,7 +88,7 @@ when "openbsd"
     it { should be_mode 755 }
     it { should be_owned_by default_user }
     it { should be_grouped_into default_group }
-    its(:content) { should match(/^daemon="\/usr\/local\/sbin\/radium"$/) }
+    its(:content) { should match(%r{^daemon="/usr/local/sbin/radium"$}) }
   end
 when "redhat"
   describe file("/etc/sysconfig/radium") do
@@ -124,7 +124,7 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
-  describe command("ra -S 127.0.0.1:#{ p } -N 1 -M man") do
+  describe command("ra -S 127.0.0.1:#{p} -N 1 -M man") do
     its(:stdout) { should match(/^\s+StartTime\s+Flgs\s+Proto\s+SrcAddr\s+Sport\s+Dir\s+DstAddr\s+Dport\s+TotPkts\s+TotBytes\s+State/) }
     its(:stderr) { should eq "" }
     its(:exit_status) { should eq 0 }
